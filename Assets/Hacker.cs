5130bdbecac12709448ddf45e8c196b198954dd9
Scott Abbott <scott.abbott.a@gmail.com>
2018-05-07 17:20:34 -0600

    added password checking, just a placeholder

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -7,6 +7,7 @@ public class Hacker : MonoBehaviour {
 	int level;
 	enum Screen {MainMenu, Password, Win};
 	Screen currentScreen;
+	string password;
 
 	// Use this for initialization
 	void Start () {
@@ -30,24 +31,34 @@ public class Hacker : MonoBehaviour {
 		else if (currentScreen == Screen.MainMenu){
 			RunMainMenu(input);
 		}
+		else if (currentScreen == Screen.Password){
+			CheckPassword(input);
+		}
 	}
 	void RunMainMenu(string input){
 		if(input == "1"){
 			level = 1;
+			password = "wifi";
 			StartGame();
 		}
 		else if(input == "2"){
 			level = 2;
+			password = "account";			
 			StartGame();
 		}
 		else if(input == "007"){
 			Terminal.WriteLine("Please select a level Mr. Bond");
 		}
 		else Terminal.WriteLine("Please select a level");
-		}
+	}
 
 	void StartGame(){
 		currentScreen = Screen.Password;					
 		Terminal.WriteLine("You have chosen level " + level);
+	}
+	void CheckPassword(string input){
+		if (input == password){
+			Terminal.WriteLine("GREAT JOB, YOU'RE IN!!");
+		} else Terminal.WriteLine("Incorect password");
 	}
 }
